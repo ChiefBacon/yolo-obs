6feb625cef0728c79d5f22b05ce68a0b9b898e04
ChiefBacon <tandcclarkkids@gmail.com>
2025-05-23 18:21:33 -0400

    Remove unused home assistant connectivity

--- src/main.py
+++ src/main.py
@@ -11,23 +11,12 @@ import torch
 from colorama import Fore, Back
 import configparser
 
-config = configparser.ConfigParser()
+config = configparser.ConfigParser(inline_comment_prefixes=("#", ";"))
 config.read('yolo-obs-cfg.ini')
 
 print(config.sections())
 
 print(Fore.CYAN+"Preparing... ", end=Fore.RESET)
-
-# MQTT Config
-if config.getboolean("config.mqtt", "Enabled"):
-    mqtt_enabled = True
-    broker = config.get("config.mqtt", "Broker")
-    mq_port = config.get("config.mqtt", "Port")
-    client_id = "yolo-obs"
-    mq_username = config.get("config.mqtt", "Username")
-    mq_password = config.get("config.mqtt", "Password")
-else:
-    mqtt_enabled = False
 
 # Configuration
 host = config.get("config.obs", "Host")
@@ -36,82 +25,11 @@ password = config.get("config.obs", "Password")
 
 if config.getboolean("config.ntfy", "Enabled"):
     notification_url = config.get("config.ntfy", "URL")
+    notification_topic = config.get("config.ntfy", "Topic")
+    notification_title = config.get("config.ntfy", "Title")
     ntfy_enabled = True
 else:
     ntfy_enabled = False
-
-device_info = {"device": {
-    "identifiers":[
-        "yolobs"
-    ],
-    "name":"YOLO OBS",
-    "manufacturer":"github.com/ChiefBacon",
-    "model":"YoloObs",
-    "serial_number":"yolo_239894803290",
-    }
-}
-
-detections_sensor_setup = json.dumps({
-    "name": "Animal Detections",
-    "icon": "mdi:dog-side",
-    "state_topic": "homeassistant/sensor/yolo-obs/detections/value",
-    "unique_id": "sensor.yolo-obs_detections",
-} | device_info)
-
-camera_active_setup = json.dumps({
-    "name": "Camera Status",
-    "icon": "mdi:camera",
-    "state_topic": "homeassistant/binary_sensor/yolo-obs/camera_active/value",
-    "unique_id": "binary_sensor.yolo-obs_camera_active",
-} | device_info)
-
-privacy_screen_active_setup = json.dumps({
-    "name": "Privacy Screen",
-    "icon": "mdi:eye-off",
-    "state_topic": "homeassistant/binary_sensor/yolo-obs/privacy/value",
-    "unique_id": "binary_sensor.yolo-obs_privacy_mode",
-} | device_info)
-
-animal_detected_setup = json.dumps({
-    "name": "Animal Presence",
-    "icon": "mdi:dog",
-    "device_class": "occupancy",
-    "state_topic": "homeassistant/binary_sensor/yolo-obs/animal_detected/value",
-    "unique_id": "binary_sensor.yolo-obs_animal_seen",
-} | device_info)
-
-if mqtt_enabled:
-    def connect_mqtt():
-        def on_connect(client, userdata, flags, rc):
-            if rc == 0: print("Connected to MQTT!")
-            else: print("Failed to connect to MQTT")
-        client = mqtt.Client()
-        client.username_pw_set(mq_username, mq_password)
-        client.on_connect = on_connect
-        client.connect(broker, mq_port)
-        return client
-
-
-    def publish(client, topic, msg):
-        result = client.publish(topic, msg)
-        status = result[0]
-        if status != 0: print(f"Failed to send message to {topic}")
-
-
-    print(Fore.GREEN+"Done!"+Fore.RESET)
-
-    print("Connecting To MQTT...")
-    mqtt_client = connect_mqtt()
-
-    publish(mqtt_client, 'homeassistant/sensor/yolo-obs/detections/config', detections_sensor_setup)
-    publish(mqtt_client, 'homeassistant/binary_sensor/yolo-obs/camera_active/config', camera_active_setup)
-    publish(mqtt_client, 'homeassistant/binary_sensor/yolo-obs/privacy/config', privacy_screen_active_setup)
-    publish(mqtt_client, 'homeassistant/binary_sensor/yolo-obs/animal_seen/config', animal_detected_setup)
-
-    publish(mqtt_client, 'homeassistant/sensor/yolo-obs/detections/value', 0)
-    publish(mqtt_client, 'homeassistant/binary_sensor/yolo-obs/camera_active/value', "OFF")
-    publish(mqtt_client, 'homeassistant/binary_sensor/yolo-obs/privacy/value', "OFF")
-    publish(mqtt_client, 'homeassistant/binary_sensor/yolo-obs/animal_seen/value', "OFF")
 
 # Connect to websockets
 print(f'{Fore.CYAN}Connecting to OBS... {Fore.RESET}', end="")
@@ -132,6 +50,8 @@ prev_privacy_screen = False
 scissors_in_frame = False
 time_scissors_last_seen = datetime.datetime.now()
 detection_objects = config.get("config.ai", "DetectionObjects").split(",")
+scene_height = int(config.get("config.ai", "SceneHeight"))
+scene_width = int(config.get("config.ai", "SceneWidth"))
 
 # Colors for drawing boxes
 blue = (255, 0, 0)
@@ -155,7 +75,6 @@ print("Camera Initialized!")
 print("Starting Up")
 while True:
     start = time.time()
-    if mqtt_enabled: publish(mqtt_client, "homeassistant/sensor/yolo-obs/detections/value", target_detections)
     target_in_frame = False
     scissors_in_frame = False
 
@@ -163,11 +82,9 @@ while True:
         prev_privacy_screen = privacy_screen
         if privacy_screen:
             print("PRIVACY ACTIVE")
-            publish(mqtt_client, "homeassistant/binary_sensor/yolo-obs/privacy/value", "ON")
             ws.call(requests.SetCurrentProgramScene(sceneName="Paused"))
         else:
             print("NORMAL OPERATION")
-            publish(mqtt_client, "homeassistant/binary_sensor/yolo-obs/privacy/value", "OFF")
             ws.call(requests.SetCurrentProgramScene(sceneName="Away"))
 
     if prev_cam_status != cam_status and not privacy_screen:
@@ -175,15 +92,11 @@ while True:
         if cam_status:
             print("CAM ON")
             target_detections += 1
-            if mqtt_enabled:
-                publish(mqtt_client, "homeassistant/sensor/yolo-obs/detections/value", target_detections)
-                publish(mqtt_client, "homeassistant/binary_sensor/yolo-obs/camera_active/value", "ON")
             if ntfy_enabled:
-                post(notification_url, data="Act NOW: Dog has been detected!", headers={"Title": "Walter CAM LIVE", "Actions": "http, Open Stream, http://10.1.8.26"})
+                post(notification_url+notification_topic, data="Object has been detected!", headers={"Title": notification_title, "Priority": "high"})
             ws.call(requests.SetCurrentProgramScene(sceneName="Cam 2"))
         else:
             print("CAM OFF")
-            publish(mqtt_client, "homeassistant/binary_sensor/yolo-obs/camera_active/value", "OFF")
             ws.call(requests.SetCurrentProgramScene(sceneName="Away"))
             ws.call(requests.SetSceneItemTransform(sceneName="Cam 2", sceneItemId=14, sceneItemTransform={"cropLeft": 0.0, "cropRight": 0.0, "cropTop": 0.0, "cropBottom": 0.0, "scaleX": 1.0, "scaleY": 1.0, "positionX": 0.0, "positionY": 0.0}))
 
@@ -191,12 +104,8 @@ while True:
         prev_target_present = target_present
         if target_present:
             cam_status = True
-            if mqtt_enabled:
-                publish(mqtt_client, "homeassistant/binary_sensor/yolo-obs/animal_detected/value", "ON")
         else:
             time_target_last_seen = datetime.datetime.now()
-            if mqtt_enabled:
-                publish(mqtt_client, "homeassistant/binary_sensor/yolo-obs/animal_detected/value", "OFF")
 
     ret, frame = cap.read()
     if not ret:
@@ -227,7 +136,7 @@ while True:
                     target_present = True
                     target_in_frame = True
                     time_target_last_seen = datetime.datetime.now()
-                    if config.getboolean("config.ai", "UseSmartScale"): ws.call(requests.SetSceneItemTransform(sceneName="Cam 2", sceneItemId=14, sceneItemTransform={"cropLeft": x1 - 20, "cropRight": (1920 - x2) - 20, "cropTop": y1 - 20, "cropBottom": (1080 - y2) - 20}))
+                    if config.getboolean("config.ai", "UseSmartScale"): ws.call(requests.SetSceneItemTransform(sceneName="Cam 2", sceneItemId=14, sceneItemTransform={"cropLeft": x1 - 20, "cropRight": (scene_width - x2) - 20, "cropTop": y1 - 20, "cropBottom": (scene_height - y2) - 20}))
                 elif class_name == "scissors":
                     color = red
                     time_scissors_last_seen = datetime.datetime.now()
