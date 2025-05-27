35d5f22f7e089c8e2bda9e0c05cf47584177f443
Charlie C <107328872+ChiefBacon@users.noreply.github.com>
2025-05-27 17:03:16 -0400

    simpleLogger bug fix
    Fixes a bug in simpleLogger where the file will be unable to write emojis to the log in certain circumstances.

--- src/simpleLogger.py
+++ src/simpleLogger.py
@@ -8,26 +8,26 @@ class SimpleLogger():
         self.compatibility = compatibility
 
     def logInfo(self, message: str):
-        with open(self.log_file, 'a') as f:
+        with open(self.log_file, 'a', encoding='UTF-8') as f:
             f.write(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [{'ℹ️' if not self.compatibility else "i"}] - {message}\n")
         print(f'{Fore.CYAN}[{'ℹ️' if not self.compatibility else "i"}] - {message}{Fore.RESET}')
 
     def logWarning(self, message: str):
-        with open(self.log_file, 'a') as f:
+        with open(self.log_file, 'a', encoding='UTF-8') as f:
             f.write(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [{'⚠️' if not self.compatibility else "!"}] - {message}\n")
         print(f'{Fore.YELLOW}[{'⚠️' if not self.compatibility else "!"}] - {message}{Fore.RESET}')
 
     def logError(self, message: str):
-        with open(self.log_file, 'a') as f:
+        with open(self.log_file, 'a', encoding='UTF-8') as f:
             f.write(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [{'❌' if not self.compatibility else "x"}] - {message}\n")
         print(f'{Fore.RED}[{'❌' if not self.compatibility else "x"}] - {message}{Fore.RESET}')
 
     def logSuccess(self, message: str):
-        with open(self.log_file, 'a') as f:
+        with open(self.log_file, 'a', encoding='UTF-8') as f:
             f.write(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [{'✅' if not self.compatibility else "✓"}] - {message}\n")
         print(f'{Fore.GREEN}[{'✅' if not self.compatibility else "✓"}] - {message}{Fore.RESET}')
 
-    def logDebug(self, message: str):
+    def logDebug(self, message: str, encoding='UTF-8'):
         with open(self.log_file, 'a') as f:
             f.write(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [{'🐛' if not self.compatibility else "d"}] - {message}\n")
         print(f'{Fore.MAGENTA}[{'🐛' if not self.compatibility else "d"}] - {message}{Fore.RESET}')
